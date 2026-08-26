d39e47108ed817f2ed499689402f71f135ea547f
gnomidlo <54498949+gnomidlo@users.noreply.github.com>
2026-08-26 16:53:44 +0200

    release: prepare version 0.71

--- manifest.lua
+++ manifest.lua
@@ -1,19 +1,22 @@
 return {
     name = "ChimeraVIP",
-    version = "0.70",
+    version = "0.71",
     tested_upstream = "2.6",
     raw_base = "https://raw.githubusercontent.com/gnomidlo/ChimeraVIP/main/",
     files = {
+        "loader.lua",
         "src/init.lua",
         "src/core/bootstrap.lua",
         "src/core/util.lua",
+        "src/core/help.lua",
         "src/core/updater.lua",
+        "src/integrations/chimera.lua",
         "src/theme/pastel.lua",
         "src/ui/quiet_footer.lua",
         "src/features/auto_support.lua",
         "src/features/xp_tracker.lua",
         "src/features/stats.lua",
         "src/features/progression.lua",
-        "src/ui/footer_controls.lua",
-    },
+        "src/ui/footer_controls.lua"
+    }
 }
