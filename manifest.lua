d972f520182d4e6adbbf6d6dcc70560d2a134e5a
gnomidlo <54498949+gnomidlo@users.noreply.github.com>
2026-08-26 12:45:24 +0200

    chore: add 0.67 update manifest

---
+++ manifest.lua
@@ -0,0 +1,16 @@
+return {
+    name = "ChimeraVIP",
+    version = "0.67",
+    tested_upstream = "2.6",
+    raw_base = "https://raw.githubusercontent.com/gnomidlo/ChimeraVIP/main/",
+    files = {
+        "src/init.lua",
+        "src/core/bootstrap.lua",
+        "src/core/util.lua",
+        "src/core/updater.lua",
+        "src/theme/pastel.lua",
+        "src/ui/quiet_footer.lua",
+        "src/features/auto_support.lua",
+        "src/ui/footer_controls.lua",
+    },
+}
