23bbfe21e89fb669ebd5b21353dc7b3c1e150081
gnomidlo <54498949+gnomidlo@users.noreply.github.com>
2026-08-27 23:20:36 +0200

    Release ChimeraVIP 0.77

--- manifest.lua
+++ manifest.lua
@@ -1,6 +1,6 @@
 return {
     name = "ChimeraVIP",
-    version = "0.76",
+    version = "0.77",
     tested_upstream = "2.6",
     raw_base = "https://raw.githubusercontent.com/gnomidlo/ChimeraVIP/main/",
     files = {
