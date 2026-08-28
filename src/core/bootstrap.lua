e3dddc34ec22ef8c1120c5bf8673308f5a8f2880
gnomidlo <54498949+gnomidlo@users.noreply.github.com>
2026-08-28 11:41:42 +0200

    Release ChimeraVIP 0.82

--- src/core/bootstrap.lua
+++ src/core/bootstrap.lua
@@ -3,7 +3,7 @@ chimera_overlay = chimera_overlay or chimera_vip
 
 local C = chimera_vip
 C.name = "ChimeraVIP"
-C.version = C.version or "0.81"
+C.version = C.version or "0.82"
 C.tested_upstream = "2.6"
 C.handlers = C.handlers or {}
 C.ready = false
