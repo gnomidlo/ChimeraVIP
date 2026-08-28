cd0fa9423867230cb24b2e3d3dd73bf839260755
gnomidlo <54498949+gnomidlo@users.noreply.github.com>
2026-08-28 11:41:26 +0200

    Release ChimeraVIP 0.82 with Characters module

--- manifest.lua
+++ manifest.lua
@@ -1,6 +1,6 @@
 return {
     name = "ChimeraVIP",
-    version = "0.81",
+    version = "0.82",
     tested_upstream = "2.6",
     raw_base = "https://raw.githubusercontent.com/gnomidlo/ChimeraVIP/main/",
     files = {
@@ -23,6 +23,7 @@ return {
         "src/features/xp_tracker.lua",
         "src/features/stats.lua",
         "src/features/progression.lua",
+        "src/features/characters.lua",
         "src/ui/footer_controls.lua",
         "src/ui/module_controls.lua"
     }
