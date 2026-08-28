e12082221bf9e5ac4240e7d4a64163d1368fc596
gnomidlo <54498949+gnomidlo@users.noreply.github.com>
2026-08-28 16:57:26 +0200

    Update manifest for 0.84

--- manifest.lua
+++ manifest.lua
@@ -1,6 +1,6 @@
 return {
     name = "ChimeraVIP",
-    version = "0.83",
+    version = "0.84",
     tested_upstream = "2.6",
     raw_base = "https://raw.githubusercontent.com/gnomidlo/ChimeraVIP/main/",
     files = {
