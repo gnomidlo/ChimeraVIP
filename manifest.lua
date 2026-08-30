561ff403a75b7fc8cf705058ea0be6e31dd69273
gnomidlo <54498949+gnomidlo@users.noreply.github.com>
2026-08-30 23:26:25 +0200

    Bump ChimeraVIP manifest to 0.87

--- manifest.lua
+++ manifest.lua
@@ -1,6 +1,6 @@
 return {
     name = "ChimeraVIP",
-    version = "0.86",
+    version = "0.87",
     tested_upstream = "2.6",
     raw_base = "https://raw.githubusercontent.com/gnomidlo/ChimeraVIP/main/",
     files = {
