fe2c63c18a37d9c813f2c77889c5b7d5840b49a3
gnomidlo <54498949+gnomidlo@users.noreply.github.com>
2026-09-04 00:24:19 +0200

    Update bootstrap fallback to 0.104

--- src/core/bootstrap.lua
+++ src/core/bootstrap.lua
@@ -3,7 +3,7 @@ chimera_overlay = chimera_overlay or chimera_vip
 
 local C = chimera_vip
 C.name = "ChimeraVIP"
-C.version = C.version or "0.103"
+C.version = C.version or "0.104"
 C.tested_upstream = "4.3"
 C.handlers = C.handlers or {}
 C.ready = false
